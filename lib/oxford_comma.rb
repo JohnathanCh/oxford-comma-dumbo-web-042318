818ecdc4cf5ec80ca0577e4db8d53ee0d3be4a0d
John Chriest <johnchriest@gmail.com>
2018-04-12 23:40:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,31 +5,11 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length === 3
       last = array.pop
-      text = array.join(",")
       sentence = array.push("and #{last}")
       return sentence.join(", ")
   else
     last = array.pop
-    text = array.join(",")
     sentence = array.push("and #{last}")
     return sentence.join(", ")
   end
 end
-
-
-
-
-=begin
-def oxford_comma(array)
-  if array.length == 1
-    return array.join
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length == 3
-    array.insert(-2, "and ")
-    return array.join(", ")
-  else
-    return array.join(", ")
-  end
-end
-=end
